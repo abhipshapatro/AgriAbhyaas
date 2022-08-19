9249f0ddcdbe48ef97b786eea628f36c2b9619bc
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-08-20 00:03:19 +0530

    .

---
+++ src/components/FooterCarousel.js
@@ -0,0 +1,235 @@
+import React from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import "./FooterCarousel.css"
+
+export default function LastCarousal() {
+   let setting = {
+      dot: true,
+      infinite: true,
+      speed: 600,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      cssEase: "linear"
+   }
+   return (
+      <Slider {...setting}>
+         <div className="card-wrapper card-size">
+            <div className="card">
+               <div className="card-part">
+                  <div className="card-image">
+                     <img src="images/books.png" alt="" />
+                  </div>
+                  <div className="card-description">
+                     <h5>Gate XE Thermodynamics</h5>
+                     <ul>
+                        <li>
+                           <i class="bi bi-check-circle"></i>General Aptitude
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Fluid Mechanics
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Food Technology
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Engineering Mathematics
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div className="card-review">
+                  <div className="card-rating">
+                     <h4>1.2k enrolled</h4>
+                     <div className="stars">
+                        <span>4.7</span>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <span>(12,340)</span>
+                     </div>
+                  </div>
+                  <div className="card-button">
+                     <button>View Package</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className="card-wrapper card-size">
+            <div className="card">
+               <div className="card-part">
+                  <div className="card-image">
+                     <img src="images/books.png" alt="" />
+                  </div>
+                  <div className="card-description">
+                     <h5>Gate XE Thermodynamics</h5>
+                     <ul>
+                        <li>
+                           <i class="bi bi-check-circle"></i>General Aptitude
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Fluid Mechanics
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Food Technology
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Engineering Mathematics
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div className="card-review">
+                  <div className="card-rating">
+                     <h4>1.2k enrolled</h4>
+                     <div className="stars">
+                        <span>4.7</span>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <span>(12,340)</span>
+                     </div>
+                  </div>
+                  <div className="card-button">
+                     <button>View Package</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className="card-wrapper card-size">
+            <div className="card">
+            <div className="card-part">
+                  <div className="card-image">
+                     <img src="images/books.png" alt="" />
+                  </div>
+                  <div className="card-description">
+                     <h5>Gate XE Thermodynamics</h5>
+                     <ul>
+                        <li>
+                           <i class="bi bi-check-circle"></i>General Aptitude
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Fluid Mechanics
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Food Technology
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Engineering Mathematics
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div className="card-review">
+                  <div className="card-rating">
+                     <h4>1.2k enrolled</h4>
+                     <div className="stars">
+                        <span>4.7</span>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <span>(12,340)</span>
+                     </div>
+                  </div>
+                  <div className="card-button">
+                     <button>View Package</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className="card-wrapper card-size">
+            <div className="card">
+               <div className="card-part">
+                  <div className="card-image">
+                     <img src="images/books.png" alt="" />
+                  </div>
+                  <div className="card-description">
+                     <h5>Gate XE Thermodynamics</h5>
+                     <ul>
+                        <li>
+                           <i class="bi bi-check-circle"></i>General Aptitude
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Fluid Mechanics
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Food Technology
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Engineering Mathematics
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div className="card-review">
+                  <div className="card-rating">
+                     <h4>1.2k enrolled</h4>
+                     <div className="stars">
+                        <span>4.7</span>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <span>(12,340)</span>
+                     </div>
+                  </div>
+                  <div className="card-button">
+                     <button>View Package</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className="card-wrapper card-size">
+            <div className="card">
+               <div className="card-part">
+                  <div className="card-image">
+                     <img src="images/books.png" alt="" />
+                  </div>
+                  <div className="card-description">
+                     <h5>Gate XE Thermodynamics</h5>
+                     <ul>
+                        <li>
+                           <i class="bi bi-check-circle"></i>General Aptitude
+                        </li>
+                        <li>
+                        <i class="bi bi-check-circle"></i>Fluid Mechanics
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Food Technology
+                        </li>
+                        <li>
+                           <i class="bi bi-check-circle"></i>Engineering Mathematics
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div className="card-review">
+                  <div className="card-rating">
+                     <h4>1.2k enrolled</h4>
+                     <div className="stars">
+                        <span>4.7</span>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <span>(12,340)</span>
+                     </div>
+                  </div>
+                  <div className="card-button">
+                     <button>View Package</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </Slider>
+   )
+}
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "./Carousel";
-import Dropdown from "./Dropdown";
+import FooterCarousel from "./FooterCarousel";
+
 import "./Section.css";
 
 function Section() {
@@ -68,65 +69,120 @@ function Section() {
             </div>
           </div>
         </div>
-        
+
         <div className="question-section-right">
-            <form class="quiz-form" id="quiz-form">
-              <div class="question-container">
-                <h3>1.</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error modi consequuntur suscipit maxime voluptates totam nulla o
-                  mnis molestiae earum, ut tempore? Cum, dolor. Aliquid quae eaque adipisci rerum ipsa?
-                </p>
-               </div>
-               <div className="options">
-                  <label>
-                    <input type="radio" name="question1" value="45°" />
-                    <span class="radio-value">45°</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="question1" value="90°" />
-                    <span class="radio-value">90°</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="question1" value="60°" />
-                    <span class="radio-value">60°</span>
-                  </label>
-                </div> 
-                <div className="btns">
-                  <button id="ans-sol">Answer and Solution</button>
-                  <button id="discuss">Join the discussion</button>
-                  <button id="save">Save for later</button>
-                </div>
-              <div class="question-container">
-                <h3>2.</h3>
-                <p>
-                  If a triangle has an angle of 90 degrees, what is
-                  it called?
-                </p>
-              </div>
-               <div className="options">
-                  <label>
-                    <input type="radio" name="question2" value="obtuse" />
-                    <span class="radio-value">obtuse</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="question2" value="acute" />
-                    <span class="radio-value">acute</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="question2" value="right angled" />
-                    <span class="radio-value">right angled</span>
-                  </label>
-                </div>
-              <div className="btns">
-                  <button id="ans-sol">Answer and Solution</button>
-                  <button id="discuss">Join the discussion</button>
-                  <button id="save">Save for later</button>
-                </div>
-            </form>
-        
+          <form class="quiz-form" id="quiz-form">
+            <div class="question-container">
+              <h3>1.</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                error modi consequuntur suscipit maxime voluptates totam nulla o
+                mnis molestiae earum, ut tempore? Cum, dolor. Aliquid quae eaque
+                adipisci rerum ipsa?
+              </p>
+            </div>
+            <div className="options">
+              <label>
+                <input type="radio" name="question1" value="45°" />
+                <span class="radio-value">45°</span>
+              </label>
+              <label>
+                <input type="radio" name="question1" value="90°" />
+                <span class="radio-value">90°</span>
+              </label>
+              <label>
+                <input type="radio" name="question1" value="60°" />
+                <span class="radio-value">60°</span>
+              </label>
+            </div>
+            <div className="btns">
+              <button id="ans-sol">Answer and Solution</button>
+              <button id="discuss">Join the discussion</button>
+              <button id="save">Save for later</button>
+            </div>
+            <div class="question-container">
+              <h3>2.</h3>
+              <p>
+                If a triangle has an angle of 90 degrees, what is it called?
+              </p>
+            </div>
+            <div className="options">
+              <label>
+                <input type="radio" name="question2" value="obtuse" />
+                <span class="radio-value">obtuse</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="acute" />
+                <span class="radio-value">acute</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="right angled" />
+                <span class="radio-value">right angled</span>
+              </label>
+            </div>
+            <div className="btns">
+              <button id="ans-sol">Answer and Solution</button>
+              <button id="discuss">Join the discussion</button>
+              <button id="save">Save for later</button>
+            </div>
+            <div class="question-container">
+              <h3>3.</h3>
+              <p>
+                If a triangle has an angle of 90 degrees, what is it called?
+              </p>
+            </div>
+            <div className="options">
+              <label>
+                <input type="radio" name="question2" value="obtuse" />
+                <span class="radio-value">obtuse</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="acute" />
+                <span class="radio-value">acute</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="right angled" />
+                <span class="radio-value">right angled</span>
+              </label>
+            </div>
+            <div className="btns">
+              <button id="ans-sol">Answer and Solution</button>
+              <button id="discuss">Join the discussion</button>
+              <button id="save">Save for later</button>
+            </div>
+            <div class="question-container">
+              <h3>4.</h3>
+              <p>
+                If a triangle has an angle of 90 degrees, what is it called?
+              </p>
+            </div>
+            <div className="options">
+              <label>
+                <input type="radio" name="question2" value="obtuse" />
+                <span class="radio-value">obtuse</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="acute" />
+                <span class="radio-value">acute</span>
+              </label>
+              <label>
+                <input type="radio" name="question2" value="right angled" />
+                <span class="radio-value">right angled</span>
+              </label>
+            </div>
+            <div className="btns">
+              <button id="ans-sol">Answer and Solution</button>
+              <button id="discuss">Join the discussion</button>
+              <button id="save">Save for later</button>
+            </div>
+          </form>
         </div>
       </div>
+
+      <div className="sliders container">
+            <h1 className='slider_title'>Checkout other packages</h1>
+            <FooterCarousel />
+         </div>
     </div>
   );
 }
