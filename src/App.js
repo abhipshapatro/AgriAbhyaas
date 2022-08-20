056d08f7170fabe3c5acc04f81ed68dbae62f035
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-08-20 16:09:59 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Section from './components/Section';
+import QuestionPage from './components/QuestionPage';
 
 function App() {
   return (
     <div className="home">
       {/* section page */}
-      <Section />
+      <Section path='/' />
       {/* question page */}
+      <QuestionPage path='/questionpage' />
     </div>
   );
 }
---
+++ src/components/QuestionPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuestionPage() {
+  return (
+    <div>
+      <h2>Question page</h2>
+    </div>
+  )
+}
+
+export default QuestionPage
